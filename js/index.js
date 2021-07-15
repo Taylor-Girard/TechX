512753751fc1d5639902f555363347dc360bc4e6
686taytay <686taytay@gmail.com>
2021-07-14 23:29:25 -0700

    can add items to list

--- js/index.js
+++ js/index.js
@@ -1,3 +1,21 @@
+let button = document.getElementById("submit");
+button.onclick = myFunction;
+
+
 function myFunction() {
     document.getElementById("myButton").innerHTML = "You're pretty cool <3";
-}
+};
+
+document.getElementById("form").onsubmit = function (event) {
+    event.preventDefault();
+    let languages = document.getElementById("add").value;
+
+    addToList(languages);
+};
+
+
+function addToList(item) {
+
+document.getElementById("languages").innerHTML += `<li>`+ item+ `</li>`
+
+};
